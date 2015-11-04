932d40bda29487211d9e76d978e6e53a5d95bf5f
padraic wade <padraic.waders@hotmail.com>
2015-11-04 20:31:09 +0000

    started origional fuctions

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
-$(document).ready(function(){
-	var canvas = $("#canvas")[0];
-	var ctx = canvas.getContext("2d");
-	var w = $("#canvas").width();
-	var h = $("#canvas").height();
-	
-	})
+var canvas = new Object();
+var mainSnake;
+var food;
+
+
+canvas.element = document.getElementById('canvas');
+canvas.context = canvas.element.getContext('2d');
+canvas.width = canvas.element.getAttribute('width');
+canvas.height = canvas.element.getAttribute('height');
+canvas.cellWidth = 10;
+
+canvas.redraw = function(fillColour, strokeColour){
+	// Add default canvas colour options
+	var fillColour = fillColour || 'white',
+		strokeColour = strokeColour || 'black';
+
+	this.paint(0, 0, fillColour, strokeColour, this.width, this.height);
+}
